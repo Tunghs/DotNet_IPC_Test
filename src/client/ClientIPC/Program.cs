79208909505cf155a2a61802c9c2de89b4fef75e
SeonghyeonLee <juk08843@gmail.com>
2023-03-19 22:55:59 +0900

    * 클라이언트 추가

--- src/client/ClientIPC/Program.cs
+++ src/client/ClientIPC/Program.cs
@@ -2,14 +2,32 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Security.Permissions;
+
+using RemoteObjects;
 
 namespace ClientIPC
 {
     class Program
     {
+        [STAThread]
+        [SecurityPermission(SecurityAction.Demand)]
         static void Main(string[] args)
         {
+            RemoteObject.CreateServer();
+            RemoteObject ro = new RemoteObject();
+            int nCount = 0;// IPC와 비교를 위한 숫자 변수 
+            string sStr = "";// IPC와 비교를 위한 문자 변수
+            while (true)
+            {
+                if (nCount != ro.Count)
+                {
+                    nCount = ro.Count;
+                    sStr = ro.Str;
+                    Console.WriteLine("{0}", nCount);
+                    Console.WriteLine("서버입력={0}", sStr);
+                }
+            }
         }
     }
 }
