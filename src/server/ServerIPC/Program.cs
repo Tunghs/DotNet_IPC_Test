d4357f99a4798910c3dee957a249905d0a48680f
SeonghyeonLee <juk08843@gmail.com>
2023-03-19 22:43:51 +0900

    * Server Program 생성

--- src/server/ServerIPC/Program.cs
+++ src/server/ServerIPC/Program.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Security.Permissions;
+
+using RemoteObjects;
 
 namespace ServerIPC
 {
     class Program
     {
+        [STAThread]
+        [SecurityPermission(SecurityAction.Demand)]
         static void Main(string[] args)
         {
+            RemoteObject.CreateServer();
+            RemoteObject ro = new RemoteObject();
+
+            int nCount = 0;
+            ro.Count = nCount;
+
+            while (true)
+            {
+                Console.Write("아무키나 눌러주세요 , Q=종료");
+                string tx = Console.ReadLine();
+
+                if (tx.ToUpper() == "Q")
+                {
+                    break;
+                }
+
+                nCount++;
+                ro.Count = nCount;
+                ro.Str = tx;
+
+                Console.WriteLine("카운터={0}", nCount);
+            }
         }
     }
 }
